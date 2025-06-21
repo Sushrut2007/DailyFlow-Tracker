0070f722dacf9b0cc175d624c8151a745d5e2bca
Sushrut <sushrutdeshmukh4@gmail.com>
2025-06-21 12:16:57 +0530

    Converted time to proper datatype and added summary for total time per task

--- src/analyzer.py
+++ src/analyzer.py
@@ -33,10 +33,11 @@ convert_times()
 # total time per task
 def total_time_per_task():
     df['total_time'] = (df['end_time'] - df['start_time']).dt.total_seconds() / 60
+    
+    # groupby task and calculate real total time
+    temp = df.groupby('activity_name')['total_time'].sum()
+    return temp
 
-# total_time_per_task()
+# temp = total_time_per_task()
 
 
-
-
-print(df.head())    
